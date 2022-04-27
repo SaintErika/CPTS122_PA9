ec8150d952363fa6f7eb4fb4c0dad916bf4921b2
EthLyoWSU <104379652+EthLyoWSU@users.noreply.github.com>
2022-04-26 21:07:39 -0700

    test case 1 test 1

--- position.hpp
+++ position.hpp
@@ -14,7 +14,7 @@ int checkWhite = 0, checkBlack = 0;
 
 float dx = 0, dy = 0;
 
-int board[8][8] =
+/*int board[8][8] =
 { 2, 3, 4, 5, 6, 4, 3, 2,
   1, 1, 1, 1, 1, 1, 1, 1,
   0, 0, 0, 0, 0, 0, 0, 0,
@@ -22,4 +22,22 @@ int board[8][8] =
   0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0,
  -1,-1,-1,-1,-1,-1,-1,-1,
+ -2,-3,-4,-5,-6,-4,-3,-2, };*/
+
+int board[8][8] =
+{ 2, 3, 4, 5, 6, 4, 3, 2,
+  1, 0, 1, 0, 0, 1, 1, 1,
+  0, 1, 0, 0, 0, 0, 0, 0,
+  0, 0, -1, 1, 1, 0, 0, 0,
+  0, 0, 0, -1, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+ -1,-1,0,0,-1,-1,-1,-1,
  -2,-3,-4,-5,-6,-4,-3,-2, };
+
+//pawn testing
+
+//in this test, attempt with white to take your own piece (d4 x c5) (fails)
+//then take black's piece (d4 x e5)
+//Then attempt to move pawn positioned 2 over and 3 down two spaces (pawn to b4) (fails)
+//then with black, move the pawn positioned 6 over and 1 down two spaces (pawn to f5)
+//then with white, move to en pessant (e5 x f6)
