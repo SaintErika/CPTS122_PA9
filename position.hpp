43298572006381ff7c8522598b7501d0e5d29578
EthLyoWSU <104379652+EthLyoWSU@users.noreply.github.com>
2022-04-26 21:30:06 -0700

    testing test case 2

--- position.hpp
+++ position.hpp
@@ -24,7 +24,9 @@ float dx = 0, dy = 0;
  -1,-1,-1,-1,-1,-1,-1,-1,
  -2,-3,-4,-5,-6,-4,-3,-2, };*/
 
-int board[8][8] =
+///////////////////////////////////////// testing /////////////////////////////////////////
+
+/*int board[8][8] =
 { 2, 3, 4, 5, 6, 4, 3, 2,
   1, 0, 1, 0, 0, 1, 1, 1,
   0, 1, 0, 0, 0, 0, 0, 0,
@@ -34,10 +36,31 @@ int board[8][8] =
  -1,-1,0,0,-1,-1,-1,-1,
  -2,-3,-4,-5,-6,-4,-3,-2, };
 
-//pawn testing
+//pawn testing, taking own piece, wrong turn
 
 //in this test, attempt with white to take your own piece (d4 x c5) (fails)
 //then take black's piece (d4 x e5)
 //Then attempt to move pawn positioned 2 over and 3 down two spaces (pawn to b4) (fails)
 //then with black, move the pawn positioned 6 over and 1 down two spaces (pawn to f5)
-//then with white, move to en pessant (e5 x f6)
+//then with black, attempt to move any piece, to show that it is indeed white's turn.
+//then with white, attempt to move the king's pawn up one space (pawn e3)*/
+
+int board[8][8] =
+{ 0, 0, 0, 0, 2, 6, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 5, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, -1, 0, -4, 0,
+  0, 0, 0, 0, 0,-1,-1,-1,
+  0, 0, 0, 0, 0, 0,-6, 0, };
+
+//checkmate testing
+
+//in this test, attempt with white to put the king in check by moving to d6
+//block the check with the rook (rook to e7)
+//move the pawn from e3 to e4
+//move the queen to b1
+
+
+
