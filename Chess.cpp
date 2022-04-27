a2cd4c3fb54a533083692ef0c697d815d895f5f9
EthLyoWSU <104379652+EthLyoWSU@users.noreply.github.com>
2022-04-26 21:05:53 -0700

    test case test 2

--- Chess.cpp
+++ Chess.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
-#include "positiontest1.hpp" //select for pawn testing
-//#include "position.hpp" //deselect for testing
+#include "position.hpp" //deselect for testing
 #include "defines.hpp"
 #include "Pieces.hpp"
 
